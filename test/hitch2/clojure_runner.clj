8e3a6a3e5c5bd32ae2085328a1e2808e87c83c23
Tyler Tallman <dspiteself@gmail.com>
2018-10-01 11:14:35 -0500

    run graph-tests

--- test/hitch2/clojure_runner.clj
+++ test/hitch2/clojure_runner.clj
@@ -1,7 +1,8 @@
 (ns hitch2.clojure-runner
   (:require [clojure.test :refer :all]
             [hitch2.hitch2-test]
-            [hitch2.graph-manager.atom-tests]))
+            [hitch2.graph-manager.atom-tests]
+            [hitch2.graph-test]))
 
 (defn -main [& args]
   (run-all-tests))
