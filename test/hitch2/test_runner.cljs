0d1465803804919e894ca61c569f02615294bcde
dan sutton <danielsutton01@gmail.com>
2018-10-05 15:32:23 -0500

    Remove import of deleted ns

--- test/hitch2/test_runner.cljs
+++ test/hitch2/test_runner.cljs
@@ -2,7 +2,6 @@
 (ns hitch2.test-runner
   (:require
     ;; require all the namespaces that you want to test
-    [hitch2.hitch2-test]
     [figwheel.main.testing :refer [run-tests-async]]
     [hitch2.graph-manager.atom-tests]
     [hitch2.graph-test]
