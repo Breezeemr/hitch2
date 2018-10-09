14d3d94c2f48cc508541474cd93137cf8b8e3575
Francis Avila <favila@breezeehr.com>
2018-10-09 17:48:33 -0500

    Add back test.txt and use it in http-test
    Note: does not seem to work with `clj -A:cljs-test`. Figwheel will
    serve back the html page instead of the test.txt file you asked for!
    Works fine with figwheel when not operating as a test runner.

--- test/hitch2/machine/http_test.cljs
+++ test/hitch2/machine/http_test.cljs
@@ -7,37 +7,17 @@
             [hitch2.selector-impl-registry :as reg
              :refer [registry-resolver]]))
 
-(def url "http://dummy.restapiexample.com/api/v1/employees")
-(defn deserializer [payload]
-  (let [json (.parse js/JSON payload)]
-    (first json)))
-
-(def http-call
-  (http/http "http://dummy.restapiexample.com/api/v1/employees"
-             :get
-             nil
-             deserializer
-             nil nil nil))
-
-(defn assert-valid-http-call [done graph-name [status value :as result]]
-  (let [decoded (js->clj value)]
-    (is (and (map? decoded)
-             (seq decoded))
-        graph-name))
-  (done))
-
 (def gctors
   [["Atom graph: " (fn [] (atom-gm/make-gm registry-resolver))]])
 
 (doseq [[graph-name gctor] gctors]
   (deftest simple-get-ok
-    (binding [atom-gm/*trace* true]
-      ;;(atom-gm/clear-trace!)
-      (let [graph (gctor)]
-        (async done
-               (graph/hook-sel graph
-                               (partial assert-valid-http-call done graph-name)
-                               http-call)))))
+    (let [graph (gctor)]
+      (async done
+        (graph/hook-sel graph (fn [[status value :as result]]
+                            (is (= result [:ok "cat\n"]) graph-name)
+                            (done))
+          (http/http "/test.txt" :get nil nil nil nil nil)))))
 
   (deftest simple-get-error
     (let [graph (gctor)]
@@ -49,9 +29,20 @@
 
   (deftest simple-refresh
     (testing "Additional http request is issued after a ::http/refresh command. (Must verify manually in dev console.)"
-      (let [graph (gctor)]
+      (let [graph (gctor)
+            sel   (http/http "/test.txt" :get nil nil nil nil nil)]
         (async done
-               (graph/hook-sel graph
-                               (partial assert-valid-http-call done graph-name)
-                               
-                               http-call))))))
+          (graph/hook-sel graph
+            (fn [result]
+              (is (= result [:ok "cat\n"]) (str graph-name " value before refresh"))
+
+              ;; Second hook is to ensure the selector has not been destroyed
+              ;; so the second http request is not elided somehow.
+              (graph/hook-sel graph
+                (fn [result]
+                  (is (= result [:ok "cat\n"]) (str graph-name " value after refresh, next frame"))
+                  (done))
+                sel)
+              (graph/apply-commands graph [[sel [::http/refresh]]])
+              (is (= result [:ok "cat\n"]) (str graph-name " value after refresh")))
+            sel))))))
