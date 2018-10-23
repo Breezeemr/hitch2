da0c88ea882afe045e3f3209622b921643cefaef
dan sutton <danielsutton01@gmail.com>
2018-10-23 11:50:23 -0500

    Add project.clj for artifact creation and deployment
    deps are great for running. no good story for packaging and building
    like lein. Just crank up and run:
    
    `lein deploy` and all works out.
    
    NOTE: requires lein 2.8.1 but jib requires lein 2.7.1 :(

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject com.breezeehr/hitch2 "0.1.0-SNAPSHOT"
+  :description ""
+  :url "https://github.com/Breezeemr/hitch2/tree/master/"
+  :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
+                 ["releases" {:url "s3p://breezepackages/releases" :creds :gpg}]]
+  :plugins [[lein-tools-deps "0.4.1"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]})
