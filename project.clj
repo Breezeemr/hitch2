6c559344a4891296dd0571a81e86ce6933c8d7b5
Francis Avila <favila@breezeehr.com>
2018-10-23 19:29:11 -0500

    Ensure minimum lein version

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject com.breezeehr/hitch2 "0.1.0-SNAPSHOT"
   :description ""
+  ;; lein-tools-deps plugin does not work with lein 2.7.1
+  :min-lein-version "2.8.0"
   :url "https://github.com/Breezeemr/hitch2/tree/master/"
   :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
                  ["releases" {:url "s3p://breezepackages/releases" :creds :gpg}]]
