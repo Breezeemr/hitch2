61e3ad19dc9db57de6dc290c6ba7ab20206ffb9a
Tyler Tallman <dspiteself@gmail.com>
2019-06-18 14:06:56 -0500

    add process manager in hitch 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.breezeehr/hitch2 "0.3.3"
+(defproject com.breezeehr/hitch2 "0.3.4"
   :description ""
   :url "https://github.com/Breezeemr/hitch2/tree/master/"
   :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
