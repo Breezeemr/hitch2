2af082565d8c502926e725721797eaae542079e3
Tyler Tallman <dspiteself@gmail.com>
2019-06-19 17:22:47 -0500

    release last resolved value caching.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.breezeehr/hitch2 "0.3.5-SNAPSHOT"
+(defproject com.breezeehr/hitch2 "0.3.5"
   :description ""
   :url "https://github.com/Breezeemr/hitch2/tree/master/"
   :repositories [["snapshots" {:url "s3p://breezepackages/snapshots" :creds :gpg}]
